cc70dcf6f9a1b8ebc9dd7b00550e5ae43d878e37
Daan Ferdinandusse <dferdinandusse@gmail.com>
2018-11-02 14:42:50 +0100

    end project

--- app/src/main/java/com/example/daan/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/daan/mrpotatohead/MainActivity.java
@@ -174,29 +174,7 @@ public class MainActivity extends AppCompatActivity {
             case "Shoes": image = (ImageView) findViewById(R.id.shoes);
                 break;
         }
-        /*
-        if (name.equals("Hat")){
-            image = (ImageView) findViewById(R.id.hat);
-        } else if (name.equals("Eyebrows")) {
-            image = (ImageView) findViewById(R.id.eyebrows);
-        } else if (name.equals("Nose")) {
-            image = (ImageView) findViewById(R.id.nose);
-        } else if (name.equals("Mustage")) {
-            image = (ImageView) findViewById(R.id.mustage);
-        } else if (name.equals("Arms")) {
-            image = (ImageView) findViewById(R.id.arms);
-        } else if (name.equals("Eyes")) {
-            image = (ImageView) findViewById(R.id.eyes);
-        } else if (name.equals("Glasses")) {
-            image = (ImageView) findViewById(R.id.glasses);
-        } else if (name.equals("Mouth")) {
-            image = (ImageView) findViewById(R.id.mouth);
-        } else if (name.equals("Ears")) {
-            image = (ImageView) findViewById(R.id.ears);
-        } else if (name.equals("Shoes")) {
-            image = (ImageView) findViewById(R.id.shoes);
-        }
-        */
+
         if (checkbox.isChecked()){
             image.setVisibility(View.VISIBLE);
         } else {
